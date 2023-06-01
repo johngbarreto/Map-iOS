d51158eed9da83689815fc98a9afad72aaf06f12
johnidk <johngabrielbarreto1337@gmail.com>
2023-06-01 17:08:29 -0300

    added search funcionality based on user location

--- MyMapApp/ViewController.swift
+++ MyMapApp/ViewController.swift
@@ -10,9 +10,11 @@ import MapKit
 
 class ViewController: UIViewController {
     
+    var locationManager: CLLocationManager?
+    
     lazy var mapView: MKMapView = {
         let map = MKMapView()
-        //map.showsUserLocation = true
+        map.showsUserLocation = true
         map.translatesAutoresizingMaskIntoConstraints = false
         return map
     }()
@@ -21,6 +23,7 @@ class ViewController: UIViewController {
        let txtField = UITextField()
         txtField.layer.cornerRadius = 10
         txtField.clipsToBounds = true
+        txtField.delegate = self
         txtField.backgroundColor = UIColor.white
         txtField.placeholder = "Search..."
         txtField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
@@ -32,9 +35,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        locationManagement()
         configureUI()
     }
 
+    func locationManagement() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+    }
 
     func configureUI() {
         
@@ -56,5 +68,64 @@ class ViewController: UIViewController {
         mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
     }
+    
+    private func checkLocationAuth() {
+        guard let locationManager = locationManager, let location = locationManager.location else { return }
+        
+        switch locationManager.authorizationStatus {
+            case .authorizedAlways, .authorizedWhenInUse:
+                let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 600, longitudinalMeters: 600)
+                mapView.setRegion(region, animated: true)
+            case .denied:
+                print("User needs to enable it again")
+            case .notDetermined, .restricted:
+                print("Undertemined error or restricted")
+            @unknown default:
+                print("default error")
+            
+        }
+    }
+    
+    private func findNearbyPlaces(by query: String) {
+        mapView.removeAnnotations(mapView.annotations)
+        
+        let req = MKLocalSearch.Request()
+        req.naturalLanguageQuery = query
+        req.region = mapView.region
+        
+        let search = MKLocalSearch(request: req)
+        search.start{ res, error in
+            guard let res = res, error == nil else {return}
+            print(res.mapItems)
+        }
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let text = textField.text ?? ""
+        if !text.isEmpty {
+            textField.resignFirstResponder()
+            findNearbyPlaces(by: text)
+        }
+        return true
+    }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuth()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
